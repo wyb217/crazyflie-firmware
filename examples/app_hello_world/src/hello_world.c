1a5d1b1b893d96e46c91515a3730a76f2edd6a3d
wyb217 <1838798301@qq.com>
2024-09-30 16:56:40 +0800

    cpx first successful try

--- examples/app_hello_world/src/hello_world.c
+++ examples/app_hello_world/src/hello_world.c
@@ -1,50 +1,111 @@
-/**
- * ,---------,       ____  _ __
- * |  ,-^-,  |      / __ )(_) /_______________ _____  ___
- * | (  O  ) |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
- * | / ,--´  |    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
- *    +------`   /_____/_/\__/\___/_/   \__,_/ /___/\___/
- *
- * Crazyflie control firmware
- *
- * Copyright (C) 2019 Bitcraze AB
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, in version 3.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- *
- * hello_world.c - App layer application of a simple hello world debug print every
- *   2 seconds.
- */
-
-
 #include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
 
 #include "app.h"
-
 #include "FreeRTOS.h"
 #include "task.h"
+#include "log.h"
 
-#define DEBUG_MODULE "HELLOWORLD"
+#include "uart1.h"
+#include "semphr.h"
+#include "uart_syslink.h"
+#include "timers.h"
 #include "debug.h"
+#include "log.h"
+#include "param.h"
+#include "cpx.h"
 
+#define DEBUG_MODULE "ATHENA"
 
-void appMain() {
-  DEBUG_PRINT("Waiting for activation ...\n");
+static CPXPacket_t rxPacket;
+static uint8_t rxBuffer[CPX_MAX_PAYLOAD_SIZE + CPX_HEADER_SIZE + 2]; // 包含包头和包尾
+SemaphoreHandle_t UartRxReady = NULL;
 
-  while(1) {
-    vTaskDelay(M2T(2000));
-    DEBUG_PRINT("Hello World!\n");
+void parseCPXPacket(CPXPacket_t *rxPacket, uint8_t *rxBuffer, uint32_t length)
+{
+  if (length < CPX_HEADER_SIZE + 2)
+  {
+    DEBUG_PRINT("Error: Received data length too short\n");
+    return;
+  }
+
+  // // 检查包头和包尾是否正确
+  // if (rxBuffer[0] != 0xAA || rxBuffer[length - 1] != 0x55)
+  // {
+  //   DEBUG_PRINT("Error: Invalid packet start or end marker\n");
+  //   return;
+  // }
+
+  // // 解析第一个字节为 destination 和 source
+  // rxPacket->route.destination = rxBuffer[1] & 0x07;       // 低 3 位为 destination
+  // rxPacket->route.source = (rxBuffer[1] >> 3) & 0x07;     // 中间 3 位为 source
+  // rxPacket->route.lastPacket = (rxBuffer[1] >> 6) & 0x01; // 第 6 位为 lastPacket
+
+  // // 解析第二个字节为 version 和 function
+  // rxPacket->route.version = rxBuffer[2] & 0x03;         // 低 2 位为 version
+  // rxPacket->route.function = (rxBuffer[2] >> 2) & 0x3F; // 高 6 位为 function
+
+  // 提取数据长度，减去路由头部和包头包尾
+  rxPacket->dataLength = length;
+  DEBUG_PRINT("rxPacket->dataLength: %d\n", rxPacket->dataLength);
+
+  // 复制数据到 CPX 数据包
+  memcpy(rxPacket->data, rxBuffer, rxPacket->dataLength);
+}
+
+uint32_t receiveData(uint8_t *buffer, uint32_t maxLength)
+{
+  uint32_t totalReceived = 0;
+  uint8_t byte;
+
+  // 循环接收数据，直到接收到完整的 CPX 数据包
+  while (totalReceived < maxLength)
+  {
+    if (uart1GetDataWithDefaultTimeout(&byte))
+    {
+      buffer[totalReceived++] = byte;
+
+      // 检查是否收到包尾，确保接收完整
+      if (totalReceived >= 2 && buffer[totalReceived - 1] == 0x55)
+      {
+        break; // 已经收到包尾，停止接收
+      }
+    }
+    else
+    {
+      // 接收完就停止接收
+      break;
+    }
+  }
+  return totalReceived; // 返回长度
+}
+
+void appMain()
+{
+  DEBUG_PRINT("Hello! I am the stm_athena_cpx app\n");
+  UartRxReady = xSemaphoreCreateMutex();
+  uart1Init(115200);
+
+  while (1)
+  {
+    uint32_t receivedLength = receiveData(rxBuffer, sizeof(rxBuffer));
+    if (receivedLength >= CPX_HEADER_SIZE + 2)
+    {
+      // 解包
+      parseCPXPacket(&rxPacket, rxBuffer, receivedLength);
+      DEBUG_PRINT("Received CPX data from Athena: ");
+      for (int i = 0; i < rxPacket.dataLength; i++)
+      {
+        DEBUG_PRINT("%u\t", rxPacket.data[i]);
+      }
+      DEBUG_PRINT("\n");
+    }
+    else
+    {
+      DEBUG_PRINT("Error: Received data length too short\n");
+    }
+
+    vTaskDelay(M2T(1));
   }
 }
---
+++ examples/app_uart_athena/src/.uart_athena.o.d
@@ -0,0 +1,90 @@
+uart_athena.o: \
+ /home/wyb/workspace/crazyflie-firmware/examples/app_uart_athena/src/uart_athena.c \
+ /usr/include/newlib/string.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/reent.h \
+ /usr/include/newlib/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/sys/_locale.h /usr/include/newlib/strings.h \
+ /usr/include/newlib/sys/string.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h \
+ /home/wyb/workspace/crazyflie-firmware/src/modules/interface/app.h \
+ /home/wyb/workspace/crazyflie-firmware/vendor/FreeRTOS/include/FreeRTOS.h \
+ /home/wyb/workspace/crazyflie-firmware/src/config/FreeRTOSConfig.h \
+ /home/wyb/workspace/crazyflie-firmware/src/config/config.h \
+ /home/wyb/workspace/crazyflie-firmware/src/drivers/interface/nrf24l01.h \
+ /home/wyb/workspace/crazyflie-firmware/src/drivers/interface/nRF24L01reg.h \
+ /home/wyb/workspace/crazyflie-firmware/src/config/trace.h \
+ /home/wyb/workspace/crazyflie-firmware/src/hal/interface/usec_time.h \
+ /home/wyb/workspace/crazyflie-firmware/src/utils/interface/cfassert.h \
+ /home/wyb/workspace/crazyflie-firmware/vendor/FreeRTOS/include/projdefs.h \
+ /home/wyb/workspace/crazyflie-firmware/vendor/FreeRTOS/include/portable.h \
+ /home/wyb/workspace/crazyflie-firmware/vendor/FreeRTOS/include/deprecated_definitions.h \
+ /home/wyb/workspace/crazyflie-firmware/vendor/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/wyb/workspace/crazyflie-firmware/vendor/FreeRTOS/include/mpu_wrappers.h \
+ /home/wyb/workspace/crazyflie-firmware/vendor/FreeRTOS/include/task.h \
+ /home/wyb/workspace/crazyflie-firmware/vendor/FreeRTOS/include/list.h \
+ /home/wyb/workspace/crazyflie-firmware/src/modules/interface/log.h \
+ /home/wyb/workspace/crazyflie-firmware/src/modules/interface/uart_receive.h \
+ /home/wyb/workspace/crazyflie-firmware/src/config/stm32fxxx.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/CMSIS/STM32F4xx/Include/stm32f4xx.h \
+ /home/wyb/workspace/crazyflie-firmware/vendor/CMSIS/CMSIS/Core/Include/core_cm4.h \
+ /home/wyb/workspace/crazyflie-firmware/vendor/CMSIS/CMSIS/Core/Include/cmsis_version.h \
+ /home/wyb/workspace/crazyflie-firmware/vendor/CMSIS/CMSIS/Core/Include/cmsis_compiler.h \
+ /home/wyb/workspace/crazyflie-firmware/vendor/CMSIS/CMSIS/Core/Include/cmsis_gcc.h \
+ /home/wyb/workspace/crazyflie-firmware/vendor/CMSIS/CMSIS/Core/Include/mpu_armv7.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/CMSIS/STM32F4xx/Include/system_stm32f4xx.h \
+ /home/wyb/workspace/crazyflie-firmware/src/config/stm32f4xx_conf.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_adc.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_crc.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_dma.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_exti.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_flash.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_gpio.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_i2c.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_iwdg.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_pwr.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_rcc.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_rtc.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_sdio.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_spi.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_syscfg.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_tim.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_usart.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_wwdg.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_misc.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_cryp.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_hash.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_rng.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_can.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_dac.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_dcmi.h \
+ /home/wyb/workspace/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc/stm32f4xx_fsmc.h \
+ /home/wyb/workspace/crazyflie-firmware/vendor/FreeRTOS/include/queue.h \
+ /home/wyb/workspace/crazyflie-firmware/vendor/FreeRTOS/include/task.h \
+ /home/wyb/workspace/crazyflie-firmware/vendor/FreeRTOS/include/semphr.h \
+ /home/wyb/workspace/crazyflie-firmware/vendor/FreeRTOS/include/queue.h \
+ /home/wyb/workspace/crazyflie-firmware/src/modules/interface/static_mem.h \
+ /home/wyb/workspace/crazyflie-firmware/src/drivers/interface/uart1.h \
+ /home/wyb/workspace/crazyflie-firmware/src/utils/interface/eprintf.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h \
+ /home/wyb/workspace/crazyflie-firmware/src/modules/interface/estimator/estimator_kalman.h \
+ /home/wyb/workspace/crazyflie-firmware/src/modules/interface/stabilizer_types.h \
+ /home/wyb/workspace/crazyflie-firmware/src/hal/interface/imu_types.h \
+ /home/wyb/workspace/crazyflie-firmware/src/utils/interface/lighthouse/lighthouse_types.h \
+ /home/wyb/workspace/crazyflie-firmware/src/drivers/interface/uart_syslink.h \
+ /home/wyb/workspace/crazyflie-firmware/src/modules/interface/crtp.h \
+ /home/wyb/workspace/crazyflie-firmware/src/hal/interface/syslink.h \
+ /home/wyb/workspace/crazyflie-firmware/vendor/FreeRTOS/include/timers.h \
+ /home/wyb/workspace/crazyflie-firmware/src/utils/interface/debug.h \
+ /home/wyb/workspace/crazyflie-firmware/src/config/config.h \
+ /home/wyb/workspace/crazyflie-firmware/src/modules/interface/console.h \
+ /home/wyb/workspace/crazyflie-firmware/examples/app_uart_athena/build/include/generated/autoconf.h \
+ /home/wyb/workspace/crazyflie-firmware/src/modules/interface/param.h \
+ /home/wyb/workspace/crazyflie-firmware/src/modules/interface/param_logic.h
